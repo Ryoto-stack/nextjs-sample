9ea96568593a12258109806ff305bea2a4c3b251
Ryoto-stack <escalakent18@gmail.com>
2024-02-21 21:11:29 +0800

    first commit

---
+++ src/app/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TableSample from "@/components/TableSample"
+
+export default function page() {
+  return (
+    <main>
+        <h1 className='text-5xl text-center py-6'> INVOICES </h1>
+        <TableSample/>
+    </main>
+  )
+}
