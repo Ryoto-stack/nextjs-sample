ca22b3cdddea019de348fb14215505c37d129f2e
Ryoto-stack <escalakent18@gmail.com>
2024-02-21 21:18:15 +0800

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import TableSample from "@/components/TableSample"
 export default function Home() {
   return (
     <main className="max-w-[1110px] w-full mx-auto">
+      <h1 className='text-5xl text-center py-6'> HOME PAGE </h1>
       <TableSample/>
     </main>
   );
